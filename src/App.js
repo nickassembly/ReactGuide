4a2dd4664edefde90e6e71ff8eaf135751f96ff1
Nick Guerra <nickg@maliciousgames.com>
2020-09-03 09:25:55 -0500

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // Guide setup test
@@ -8,13 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a react app</h1>
       </div>
     );
   }
