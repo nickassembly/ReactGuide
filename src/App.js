af3d6fd0c67048f47270cd6d117a9c6dfcdf5caa
Nick Guerra <nickg@maliciousgames.com>
2020-09-07 20:29:59 -0500

    added state and props to person

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import Person from './Person/Person';
 // Guide setup test
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Nick', age: 41},
+      {name: 'Keller', age: 99},
+      {name: 'ummm', age: 14},
+    ],
+    otherState: 'Some other value',
+  };
+
+  switchNameHandler = () => {
+    console.log('Was clicked..');
+    this.setState({
+      persons: [
+        {name: 'bobba fet', age: 41},
+        {name: 'Keller', age: 99},
+        {name: 'erssssss', age: 14},
+      ],
+    });
+  };
+
   render() {
     return (
       <div className='App'>
         <h1>Hi, I'm a react app</h1>
         <p>This is really working!</p>
-        <Person name='Nick' age='41' />
-        <Person name='Max' age='28'>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies: Racing
         </Person>
-        <Person name='Keller' age='57' />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
