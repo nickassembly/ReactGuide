015b9d2049d68a408afb53ca1a3933fb7c9e0d10
Nick Guerra <nickg@maliciousgames.com>
2020-09-15 19:03:58 -0500

    moved person to list

--- src/App.js
+++ src/App.js
@@ -47,6 +47,18 @@ class App extends Component {
       cursor: 'pointer',
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a react app</h1>
@@ -54,19 +66,7 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'KellerCorpusCapris')}
-              changed={this.nameChangedHandler}>
-              My Hobbies: Racing
-            </Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
