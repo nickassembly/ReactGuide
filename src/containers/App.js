48d6cf801dd5cf2149730f3c75cd677e0f4af35c
Nick Guerra <nickg@maliciousgames.com>
2020-09-29 19:01:12 -0500

    added lifecycle hook examples

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {id: '1', name: 'Nick', age: 41},
@@ -13,6 +18,15 @@ class App extends Component {
     otherState: 'Some other value',
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -45,6 +59,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
