a96218fdec4e8b8249fb1c2494c9aa57b5893d13
Nick Guerra <nickg@maliciousgames.com>
2020-10-07 07:26:30 -0500

    added use Ref example

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,19 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  const toggleButtonRef = useRef(null);
+
   useEffect(() => {
     console.log('[Cockpit.js] use effect');
     // Http request...
-    setTimeout(() => {
-      alert('Saved data to cloud');
-    }, 1000);
+    // setTimeout(() => {
+    //   alert('Saved data to cloud');
+    // }, 1000);
+
+    toggleButtonRef.current.click();
+
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
@@ -43,7 +48,7 @@ const cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleButtonRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import WithClass from '../../../hoc/WithClass';
-
 import Auxillary from '../../../hoc/Auxillary';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -13,10 +24,25 @@ class Person extends Component {
           I'm {this.props.name} and I am {this.props.age} years old
         </p>
         <p>{this.props.children}</p>
-        <input type='text' onChange={this.props.changed} value={this.props.name} />
+        <input
+          type='text'
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </WithClass>
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 
 export default Person;
